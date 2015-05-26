21eb688b5d970bdc68021245e990228a0eb6ed45
meryH <meryann68@gmail.com>
2015-05-26 00:03:51 -0600

    Connecting /customers/{id} with ver.html

--- initial/src/main/java/example/controllers/CustomerController.java
+++ initial/src/main/java/example/controllers/CustomerController.java
@@ -2,11 +2,7 @@ package example.controllers;
 
 import example.dao.CustomerRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import org.springframework.ui.Model;
@@ -20,20 +16,19 @@ public class CustomerController {
     private CustomerRepository customers;
 
 
-    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET)
-    public Customer greetings(@PathVariable("id") Long id) {
-        /*Customer customer = new Customer();
-        customer.setName("Martha");
-        customer.setLastname("Fuentes");
-        return customer;*/
-        return customers.getCustomer(id);
+    @RequestMapping("/customer/{id}")
+    //public String ver(@PathVariable("id") Long id) {
+    public String ver(@PathVariable("id") Long id, Model model) {
+        Customer customerData = customers.getCustomer(id);
+        model.addAttribute("customer", customerData);
+        return "customer/ver";
     }
 
     //@RequestMapping(value = "/customer", method = RequestMethod.GET)
     @RequestMapping("/customer")
     public String listar(Model model) {
-        List<Customer> allCostumers = customers.getCustomers();
-        model.addAttribute("customers", allCostumers);
+        List<Customer> allCustumers = customers.getCustomers();
+        model.addAttribute("customers", allCustumers);
         return "customer/listar";
     }
 
